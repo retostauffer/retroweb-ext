412f43b390ff56530e2f86af3bbaf9e5ec876508
Reto Stauffer <reto.stauffer@uibk.ac.at>
2022-09-14 16:36:26 +0200

    fix: Adding rules to webpack fonts; not yet working with quarto (includes missing when rendering)

--- src/js/rotorweb.js
+++ src/js/rotorweb.js
@@ -2,8 +2,11 @@
 // Loading jquery
 let $ = require("jquery");
 
-// Adding fontawesome (free version)
-require("@fortawesome/fontawesome-free");
+///// Adding fontawesome (free version)
+///require("@fortawesome/fontawesome-free");
+
+// Fontawesome support (free version)
+require("../../node_modules/@fortawesome/fontawesome-free/css/all.min.css");
 
 // Loading rotorweb webheader
 require("./webheader.js");
--- webpack.conf.js
+++ webpack.conf.js
@@ -3,35 +3,43 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        'rotorweb': path.resolve(__dirname, "./src/rotorweb.js")
+        'rotorweb': path.resolve(__dirname, "./src/js/rotorweb.js")
     },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name]-bundle.min.js"
     },
-    //module: {
-    //    rules: [
-    //        {
-    //            test: /\.css$/,
-    //            use: ["style-loader", "css-loader"]
-    //        },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(woff|woff2|ttf)(\?[a-z0-9=.]+)?$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        path: path.resolve(__dirname, "dist"),
+                        outputPath: "../fonts" // not really used but required for processing
+                    }
+                }]
+            }
+        ] // end rules
+    },
+    // Performance args to ensure not exceeding recommended file size (fonts)
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    }
+
+
+};
+
+    // trash
     //        {
     //            test: /\.scss$/,
     //            use: ["style-loader", "css-loader", "scss-loader"]
     //        },
-    //        {
-    //            test: /\.(woff|woff2|ttf)(\?[a-z0-9=.]+)?$/,
-    //            use: [{
-    //                loader: "file-loader",
-    //                options: {
-    //                    name: "[name].[ext]",
-    //                    path: path.resolve(__dirname, "fonts"),
-    //                    outputPath: "../fonts" // not really used but required for processing
-    //                }
-    //            }]
-    //        }
-    //    ]
-    //}
-
-};
-
