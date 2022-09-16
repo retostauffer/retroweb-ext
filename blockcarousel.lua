f87c5823228583bed9debd643e21cdfc2902cbf1
Reto Stauffer <reto.stauffer@uibk.ac.at>
2022-09-16 10:12:40 +0200

    feat: Adding support for user specified bootstrap5 carousel options

--- blockcarousel.lua
+++ blockcarousel.lua
@@ -7,11 +7,49 @@ function blockcarousel(args, kwargs)
         return res
     end
 
+    -- parsing bootstrap 5.0 carousel options
+    -- Define defaults:
+    local bs_ride     = "carousel"
+    local bs_interval = 5000
+    local bs_keyboard = "false"
+    local bs_pause    = "hover"
+    local bs_wrap     = "true"
+    local bs_touch    = "true"
+    -- Parsing user options (if set)
+    for key,val in pairs(kwargs) do
+        if key == "interval" then
+            bs_interval = pandoc.utils.stringify(val)
+        elseif key == "keyboard" then
+            if pandoc.utils.stringify(val) == "true" then
+                bs_keyboard = "true"
+            end
+        elseif key == "pause" then
+            bs_pause = pandoc.utils.stringify(val)
+        elseif key == "ride" then
+            bs_ride = pandoc.utils.stringify(val)
+        elseif key == "wrap" then
+            if pandoc.utils.stringify(val) == "false" then
+                bs_wrap = "false"
+            end
+        elseif key == "touch" then
+            if pandoc.utils.stringify(val) == "false" then
+                bs_touch = "false"
+            end
+        end
+    end
+
+    -- take custom HTML element ID if specified, else draw a random ID
     local id = pandoc.utils.stringify(kwargs["id"])
     if #id == 0 then id = random_id(6); end
 
     -- Building bootstrap carousel
-    local div_head = "<div id=\"" .. id .. "\" class=\"carousel slide\" data-bs-ride=\"carousel\">" ..
+    local div_head = "<div id=\"" .. id .. "\" class=\"carousel slide\"" ..
+                     " data-bs-ride=\"" .. bs_ride .. "\"" ..
+                     " data-bs-interval=\"" .. bs_interval .. "\"" ..
+                     " data-bs-keyboard=\"" .. bs_keyboard .. "\"" ..
+                     " data-bs-pause=\"" .. bs_pause .. "\"" ..
+                     " data-bs-wrap=\"" .. bs_wrap .. "\"" ..
+                     " data-bs-touch=\"" .. bs_touch .. "\">" ..
                      "  <div class=\"carousel-inner\">"
     local div_tail = "  </div>" ..
                      "  <button class=\"carousel-control-prev\" type=\"button\" data-bs-target=\"#" .. id .. "\" data-bs-slide=\"prev\">" ..
