ce8daaedd1ce2f4937bdb9072f97231426f6360d
Reto Stauffer <reto.stauffer@uibk.ac.at>
2024-08-28 14:34:42 +0200

    fix: Adding css

--- _extensions/quarto-ext/rotorweb-ext/webpack.conf.js
+++ _extensions/quarto-ext/rotorweb-ext/webpack.conf.js
@@ -23,32 +23,7 @@ module.exports = {
                 test: /\.scss$/,
                 loader: 'sass-loader'
             },
- //           {
- //               //test: /\.(woff|woff2|ttf)(\?[a-z0-9=.]+)?$/,
- //               test: /\.(woff|woff2|ttf)$/,
- //               use: [{
- //                   loader: "url-loader",
- //                   options: {
- //                       name: "[name].[ext]",
- //                       path: path.resolve(__dirname, "dist"),
- //                       outputPath: "../fonts" // not really used but required for processing
- //                   }
- //               }]
- //           }
-        ] // end rules
-    },
-    // Performance args to ensure not exceeding recommended file size (fonts)
-//    performance: {
-//        hints: false,
-//        maxEntrypointSize: 512000,
-//        maxAssetSize: 512000
-//    }
-
-
+        ]
+    }
 };
 
-    // trash
-    //        {
-    //            test: /\.scss$/,
-    //            use: ["style-loader", "css-loader", "scss-loader"]
-    //        },
